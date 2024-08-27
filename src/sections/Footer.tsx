22657ee1d05000bc7e397347b64190703654beff
MedKtite <ktite.m3@gmail.com>
2024-08-27 17:34:19 +0100

    Animation update

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <footer className='py-5 border-t border-white/15'>
       <div className='container'>
-        <div className='flex flex-col lg:flex-row gap-8'>
+        <div className='flex flex-col lg:flex-row lg:items-center gap-8'>
           <div className='flex gap-2 items-center lg:flex-1'>
             <Logo className="h-6 w-6" alt="Logo" />
             <div className='font-medium'>AI Startup Landing Page</div>
