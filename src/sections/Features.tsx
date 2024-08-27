6f151e5314f85a0c8aa59a535624a974f8db0628
MedKtite <ktite.m3@gmail.com>
2024-08-27 18:47:55 +0100

    Update

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,7 +3,7 @@
 import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from '@/assets/product-image.png';
-import { useEffect, useRef } from "react";
+import { ComponentProps, ComponentPropsWithRef, useEffect, useRef, useState } from "react";
 import { animate, motion, useMotionTemplate, useMotionValue, ValueAnimationTransition } from "framer-motion";
 
 const tabs = [
@@ -33,7 +33,10 @@ const tabs = [
   },
 ];
 
-const FeatureTab = (tab: typeof tabs[number]) => {
+const FeatureTab = (
+  props: typeof tabs[number] & ComponentPropsWithRef<'div'> & { selected: boolean }
+) => {
+  const tabRef = useRef<HTMLDivElement>(null);
   const dotlottieRef = useRef<DotLottieCommonPlayer>(null);
 
   const xPrecentage = useMotionValue(0);
@@ -41,7 +44,13 @@ const FeatureTab = (tab: typeof tabs[number]) => {
 
   const maskImage = useMotionTemplate `radial-gradient(80px 80px at ${xPrecentage}% ${yPrecenntage}%,black,transparent`;
   useEffect( () => {
+    if (!tabRef.current) return;
+    const {height, width} = tabRef.current?.getBoundingClientRect();
+    const circumfrence = height * 2 + width * 2;
+
+    const times = [0, width / circumfrence, (width + height) / circumfrence, (width * 2 + height) / circumfrence, 1]
     const options: ValueAnimationTransition = {
+      times,
       duration:4,
       repeat: Infinity,
       ease: 'linear',
@@ -57,17 +66,25 @@ const FeatureTab = (tab: typeof tabs[number]) => {
     dotlottieRef.current.play();
   };
   return (
-    <div onMouseEnter={handleTabHover} className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative">
-      <motion.div
-      style={{
-        maskImage,
-      }}
-      className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl"></motion.div>
+    <div 
+    ref={tabRef}
+    onMouseEnter={handleTabHover} 
+    className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative"
+    onClick={props.onClick}
+    >
+      {props.selected && (
+              <motion.div
+              style={{
+                maskImage,
+              }}
+              className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl"></motion.div>
+      )}
+
       <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-        <DotLottiePlayer ref={dotlottieRef} src={tab.icon} className="h-5 w-5" autoplay />
+        <DotLottiePlayer ref={dotlottieRef} src={props.icon} className="h-5 w-5" autoplay />
       </div>
-      <div className="font-medium"> {tab.title} </div>
-      {tab.isNew && (
+      <div className="font-medium"> {props.title} </div>
+      {props.isNew && (
         <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
           new
         </div>
@@ -77,6 +94,7 @@ const FeatureTab = (tab: typeof tabs[number]) => {
 };
 
 export const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <section className="py-20 md:py-24">
       <div className="container">
@@ -85,8 +103,12 @@ export const Features = () => {
           From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
         </p>
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
-          {tabs.map(tab => (
-            <FeatureTab {...tab} key={tab.title} />
+          {tabs.map((tab, tabIndex) => (
+            <FeatureTab 
+            {...tab} 
+            selected={(selectedTab === tabIndex)}
+              onClick={() => setSelectedTab(tabIndex)} 
+              key={tab.title} />
           ))}
         </div>
         <div className="border border-white/20 p-2.5 rounded-xl mt-3">
